30997fd6286009e1911f010e3b30564e8b2deedb
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-08-24 03:24:45 +0200

    Prepare dev env

---
+++ src/main/java/me/pajic/smbs/ClientMain.java
@@ -0,0 +1,10 @@
+package me.pajic.smbs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientMain implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/pajic/smbs/Main.java
@@ -0,0 +1,10 @@
+package me.pajic.smbs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
