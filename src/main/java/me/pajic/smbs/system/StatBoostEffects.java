e6beff19540cdc861859e0b52b04b099fad0d085
pajic <32738446+pajicadvance@users.noreply.github.com>
2025-08-25 23:53:02 +0200

    Cleanup

--- src/main/java/me/pajic/smbs/system/StatBoostEffects.java
+++ src/main/java/me/pajic/smbs/system/StatBoostEffects.java
@@ -1,6 +1,7 @@
 package me.pajic.smbs.system;
 
 import me.pajic.smbs.Main;
+import me.pajic.smbs.util.Buffs;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.effect.MobEffect;
@@ -13,7 +14,7 @@ public class StatBoostEffects {
     public static void init() {
         Registry.register(
                 BuiltInRegistries.MOB_EFFECT,
-                Main.id("knockback_resist"),
+                Buffs.KNOCKBACK_RESISTANCE,
                 new MobEffect(MobEffectCategory.BENEFICIAL, 0x8a8a8a).addAttributeModifier(
                         Attributes.KNOCKBACK_RESISTANCE,
                         Main.id("effect.knockback_resist"),
@@ -22,7 +23,7 @@ public class StatBoostEffects {
         );
         Registry.register(
                 BuiltInRegistries.MOB_EFFECT,
-                Main.id("attack_knockback"),
+                Buffs.KNOCKBACK_BOOST,
                 new MobEffect(MobEffectCategory.BENEFICIAL, 0x8a8a8a).addAttributeModifier(
                         Attributes.ATTACK_KNOCKBACK,
                         Main.id("effect.attack_knockback"),
@@ -31,7 +32,7 @@ public class StatBoostEffects {
         );
         Registry.register(
                 BuiltInRegistries.MOB_EFFECT,
-                Main.id("follow_range"),
+                Buffs.FARSIGHT,
                 new MobEffect(MobEffectCategory.BENEFICIAL, 0x8a8a8a).addAttributeModifier(
                         Attributes.FOLLOW_RANGE,
                         Main.id("effect.follow_range"),
@@ -40,7 +41,7 @@ public class StatBoostEffects {
         );
         Registry.register(
                 BuiltInRegistries.MOB_EFFECT,
-                Main.id("scale"),
+                Buffs.SIZE_BOOST,
                 new MobEffect(MobEffectCategory.BENEFICIAL, 0x8a8a8a).addAttributeModifier(
                         Attributes.SCALE,
                         Main.id("effect.scale"),
